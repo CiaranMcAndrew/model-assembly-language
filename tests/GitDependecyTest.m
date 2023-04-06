cf7d190bf09573052a83e7602d4351a72f61b961
CiaranMcAndrew <ciaranmc@gmail.com>
2023-04-06 22:23:11 +0100

    Added timers to test cases

--- tests/GitDependecyTest.m
+++ tests/GitDependecyTest.m
@@ -6,15 +6,28 @@ classdef GitDependecyTest < matlab.unittest.TestCase
 
     methods (TestClassSetup)
         % Shared setup for the entire test class
+        function startTimer(testCase)
+            tic
+        end
     end
 
     methods (TestMethodSetup)
         % Setup for each test
+        function reportSetupTime(testCase)
+            toc
+        end
+
         function createTempDirectory(testCase)
             mkdir(testCase.tmpDir);
             testCase.addTeardown(@(x) rmdir(x, 's'), testCase.tmpDir)
         end
 
+    end
+
+    methods (TestMethodTeardown)
+        function reportTeardownTime(testCase)
+            toc
+        end
     end
     
     methods (Test)
